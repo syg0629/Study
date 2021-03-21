d797335acdb37465a6a97dc8c3880f0d0c236728
syg0629 <syg0629@hanmail.net>
2021-03-21 23:09:30 +0900

    20210321

---
+++ Java/NewLec_JavaPrj/src/ex13/io/control6/continue_break/Program.java
@@ -0,0 +1,13 @@
+package ex13.io.control6.continue_break;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("값을 sp로 구분해서 5개 이상 입력하세요:");
+		sc.nextInt();
+	}
+}
